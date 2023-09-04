e0ee2a94bf7ec6d7408ded64d12042928d71f5ae
CanRahulCode <avulapatirahul@gmail.com>
2023-09-04 22:51:58 +0530

    hmm

---
+++ CountVowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class CountVowels {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.print("Enter the string:");
+		String s = scn.nextLine();
+		System.out.print("Vowels are:"+count(s));
+		}
+	public static int count(String s){
+		int count = 0;
+		for(int i = 0; i < s.length(); i++){
+			char ch = s.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+				count++;
+				}
+			}
+		return count;
+    }
+
+}
+/*
+ Counting of  vowels from the String.
+ */
