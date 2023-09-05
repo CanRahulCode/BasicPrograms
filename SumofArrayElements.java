44f410b28fe2aefe47592ca69e225d89b12e0659
CanRahulCode <avulapatirahul@gmail.com>
2023-09-05 23:07:32 +0530

    sum of the array elements

---
+++ SumofArrayElements.java
@@ -0,0 +1,15 @@
+public class SumofArrayElements {
+
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4};
+		int sum=0;
+		for(int i=0;i< arr.length;i++) {
+			sum+=arr[i];
+		}
+		System.out.println("Sum of the array is:"+sum);
+	}
+
+}
+/* 
+ To find the Sum of Array Elements which are present array.
+ */
