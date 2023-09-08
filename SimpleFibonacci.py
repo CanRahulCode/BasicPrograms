268f48a70d91e57504c6ad9c3b6e8529312002f7
CanRahulCode <avulapatirahul@gmail.com>
2023-09-08 23:19:11 +0530

    Simple Fibonacci using iteration

---
+++ SimpleFibonacci.py
@@ -0,0 +1,15 @@
+num = 10
+n1, n2 = 0, 1
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+
+print()
+
+''''
+Question
+# Write a program to print fibonacci series upto n terms in python.
+'''
