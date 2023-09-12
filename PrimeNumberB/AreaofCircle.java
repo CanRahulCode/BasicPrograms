6d07e518b21bb335c1d83e737834d064701cdda0
CanRahulCode <avulapatirahul@gmail.com>
2023-09-12 22:19:38 +0530

    Area of Circle

---
+++ PrimeNumberB/AreaofCircle.java
@@ -0,0 +1,15 @@
+//Java program to calculate area of a circle
+package PrimeNumberB;
+public class AreaofCircle
+{
+	public static void main(String[] args)
+	{
+			
+		double radius = 7;			
+		
+        //formula for area of a circle
+		double area = 3.14 * radius * radius;		
+		System.out.println(area);
+		
+	}
+}
